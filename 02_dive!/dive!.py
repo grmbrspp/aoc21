9fcef803ab36136e6eca17426c4440cd3c11bd32
Tim Leonhard <t_leonhard18@cs.uni-kl.de>
2021-12-02 17:09:52 +0100

    day02: use pattern matching

--- 02_dive!/dive!.py
+++ 02_dive!/dive!.py
@@ -7,20 +7,16 @@ begin = time.time()
 with open("input.txt") as file:
 	course = [line.strip() for line in file]
 
-functions = {
-	"forward": lambda n, aim: (n, 0, aim*n),
-	"up": lambda n, aim: (0, -n, 0),
-	"down": lambda n, aim: (0, n, 0)
-}
-
 h, d1, d2 = 0, 0, 0
 for instr in course:
-	fn, param = instr.split()
-	change = functions[fn](int(param), d1)
-	h, d1, d2 = \
-		h + change[0], \
-		d1 + change[1], \
-		d2 + change[2]
+	match instr.split():
+		case "forward", n:
+			h += int(n)
+			d2 += d1*int(n)
+		case "up", n:
+			d1 -= int(n)
+		case "down", n:
+			d1 += int(n)
 
 print(f"Part 1: {h * d1}")
 print(f"Part 2: {h * d2}")
