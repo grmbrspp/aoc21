4c52b0de0ba11a8a0237f05219aa19b50cb2d76f
tim <tim.leonhard@posteo.de>
2021-12-08 16:11:06 +0100

    day08: use pattern matching and refactor

--- 08_seven_segment_search/seven_segment_search.py
+++ 08_seven_segment_search/seven_segment_search.py
@@ -8,40 +8,37 @@ def get_unique_length_digit(digit_list: list, length: int) -> set:
 	digit = next(dig for dig in digit_list if len(dig) == length)
 	return set(digit)
 
-def find_digit_in_list(digit_list: list, condition) -> set:
-	digit = next(dig for dig in digit_list if condition(dig))
-	digit_list.remove(digit)
-	return digit
+def common_segment_count(a: set, b: set) -> int:
+	return len(a.intersection(b))
 
 
+unique_digits_count = 0
 outputs = []
 with open("input.txt") as file:
 	for line in file:
-		signal_patterns, out_vals = [string.strip().split() for string in line.strip().split("|")]
+		signal_patterns, out_val = [string.strip().split() for string in line.strip().split("|")]
 
-		digit_map = {}
-		digit_map["1"] = get_unique_length_digit(signal_patterns, 2)
-		digit_map["4"] = get_unique_length_digit(signal_patterns, 4)
-		digit_map["7"] = get_unique_length_digit(signal_patterns, 3)
-		digit_map["8"] = get_unique_length_digit(signal_patterns, 7)
+		output = ""
+		for digit in out_val:
+			digit = set(digit)
+			one, four = [get_unique_length_digit(signal_patterns, length) for length in (2,4)]
+			match len(digit), common_segment_count(digit, one), common_segment_count(digit, four):
+				case 2,_,_: output += "1"
+				case 3,_,_: output += "7"
+				case 4,_,_: output += "4"
+				case 7,_,_: output += "8"
+				case 5,2,_: output += "3"
+				case 5,1,2: output += "2"
+				case 5,1,3: output += "5"
+				case 6,1,_: output += "6"
+				case 6,2,3: output += "0"
+				case 6,2,4: output += "9"
 
-		five_seg_digits = [set(dig) for dig in signal_patterns if len(dig) == 5]
-		six_seg_digits = [set(dig) for dig in signal_patterns if len(dig) == 6]
+		unique_digits_count += sum(1 for digit in output if digit in {"1","4","7","8"})
+		outputs.append(int(output))
 
-		digit_map["9"] = find_digit_in_list(six_seg_digits, digit_map["4"].issubset)
-		digit_map["3"] = find_digit_in_list(five_seg_digits, digit_map["1"].issubset)
-		digit_map["5"] = find_digit_in_list(five_seg_digits, digit_map["9"].issuperset)
-		digit_map["6"] = find_digit_in_list(six_seg_digits, digit_map["5"].issubset)
-		digit_map["2"] = five_seg_digits[0]
-		digit_map["0"] = six_seg_digits[0]
-
-		inverse_digit_map = {"".join(sorted(v)): k for k, v in digit_map.items()}
-		output = "".join(inverse_digit_map["".join(sorted(val))] for val in out_vals)
-		outputs.append(output)
-
-unique_digits = ["1","4","7","8"]
-print(f"Part 1: {sum(sum(out.count(dig) for dig in unique_digits) for out in outputs)}")
-print(f"Part 2: {sum(int(o) for o in outputs)}")
+print(f"Part 1: {unique_digits_count}")
+print(f"Part 2: {sum(outputs)}")
 
 ###
 
