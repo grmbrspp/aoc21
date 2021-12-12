821e23a84629fd55115481ccb7a4d6ac45dd0731
tim <tim.leonhard@posteo.de>
2021-12-12 22:19:50 +0100

    day12: use deque for better performance and defaultdict for managing neighbors instead of networkx

--- 12_passage_pathing/passage_pathing.py
+++ 12_passage_pathing/passage_pathing.py
@@ -1,5 +1,5 @@
 import time
-import networkx
+from collections import defaultdict, deque
 
 begin = time.time()
 
@@ -14,36 +14,36 @@ def neighbour_can_be_visited(neighbour: str, path: list, special_small_cave: str
 		return True
 	return False
 
-def get_all_paths(graph, special_small_cave: str) -> set:
+def get_all_paths(special_small_cave: str) -> set:
 	finished_paths = set()
-	unfinished_paths = [["start"]]
+	unfinished_paths = deque()
+	unfinished_paths.append(["start"])
 	while unfinished_paths:
-		tmp = []
-		for path in unfinished_paths:
-			last_visited_cave = path[-1]
-			if last_visited_cave == "end":
-				path_as_str = ",".join(path)
-				finished_paths.add(path_as_str)
-				continue
-			for neighbour in graph.neighbors(last_visited_cave):
-				if neighbour_can_be_visited(neighbour, path, special_small_cave):
-					tmp.append(path + [neighbour])
-		unfinished_paths = tmp
+		path = unfinished_paths.pop()
+		last_visited_cave = path[-1]
+		if last_visited_cave == "end":
+			path_as_str = ",".join(path)
+			finished_paths.add(path_as_str)
+			continue
+		for neighbour in ADJACENCIES[last_visited_cave]:
+			if neighbour_can_be_visited(neighbour, path, special_small_cave):
+				unfinished_paths.append(path + [neighbour])
 	return finished_paths
 
 
-cave_graph = networkx.Graph()
+ADJACENCIES = defaultdict(list)
 with open("input.txt") as file:
 	for line in file:
 		cave_a, cave_b = line.strip().split("-")
-		cave_graph.add_edge(cave_a, cave_b)
+		ADJACENCIES[cave_a].append(cave_b)
+		ADJACENCIES[cave_b].append(cave_a)
 
-small_caves = [cave for cave in cave_graph.nodes()
+small_caves = [cave for cave in ADJACENCIES.keys()
 				if cave[0].islower()
 				and cave not in ["start", "end"]]
 
-p1_paths = get_all_paths(cave_graph, None)
-p2_paths = set().union(*[get_all_paths(cave_graph, cave) for cave in small_caves])
+p1_paths = get_all_paths(None)
+p2_paths = set().union(*[get_all_paths(cave) for cave in small_caves])
 
 print(f"Part 1: {len(p1_paths)}")
 print(f"Part 2: {len(p2_paths)}")
