90ac565bbd7aa77ddc1ba904e318599d1cbbeb61
tim <tim.leonhard@posteo.de>
2021-12-13 08:53:32 +0100

    day13: refactor

--- 13_transparent_origami/transparent_origami.py
+++ 13_transparent_origami/transparent_origami.py
@@ -4,17 +4,17 @@ begin = time.time()
 
 ###
 
-
 def fold(grid: set, instruction: tuple) -> set:
 	result = set()
-	modification_idx = 0 if instruction[0] == "x" else 1
-	for dot in grid:
-		if dot[modification_idx] > instruction[1]:
-			new_coord = 2*instruction[1] - dot[modification_idx]
-			new_dot = (new_coord, dot[1]) if modification_idx == 0 else (dot[0], new_coord)
-			result.add(new_dot)
+	axis, line_pos = instruction
+	for x, y in grid:
+		if axis == "x" and x > line_pos:
+			result.add((2*line_pos - x, y))
 			continue
-		result.add(dot)
+		if axis == "y" and y > line_pos:
+			result.add((x, 2*line_pos - y))
+			continue
+		result.add((x,y))
 	return result
 
 def fold_all(grid: set, instructions: list) -> set:
@@ -22,17 +22,17 @@ def fold_all(grid: set, instructions: list) -> set:
 		grid = fold(grid, instruction)
 	return grid
 
-
 def print_dots(grid: set) -> str:
 	result = ""
-	x_dimension = list(sorted(grid, key=lambda dot: dot[0]))[-1][0]
-	y_dimension = list(sorted(grid, key=lambda dot: dot[1]))[-1][1]
-	for y in range(y_dimension+1):
+	x_dimension = max(x for x,y in grid) + 1
+	y_dimension = max(y for x,y in grid) + 1
+	for y in range(y_dimension):
 		line = "\n"
-		for x in range(x_dimension+1):
+		for x in range(x_dimension):
 			line += "#" if (x,y) in grid else " "
 		result += line
 	return result
+
 
 dots = set()
 fold_instructions = []
@@ -45,9 +45,10 @@ with open("input.txt") as file:
 			instr = line.strip().split()[-1].split("=")
 			fold_instructions.append((instr[0], int(instr[1])))
 
+after_first_fold = fold(dots, fold_instructions[0])
 finished_manual = fold_all(dots, fold_instructions)
 
-print(f"Part 1: {len(fold(dots, fold_instructions[0]))}")
+print(f"Part 1: {len(after_first_fold)}")
 print(f"Part 2: {print_dots(finished_manual)}")
 
 ###
