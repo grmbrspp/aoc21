5a59ae176e7a0cd434a1729fa793fa1fa4e38b0f
tim <tim.leonhard@posteo.de>
2021-12-30 11:25:32 +0100

    day20: improve performance

--- 20_trench_map/trench_map.py
+++ 20_trench_map/trench_map.py
@@ -1,20 +1,22 @@
 import time
 from collections import defaultdict
+from functools import cache
 
 begin = time.time()
 
 ###
 
+@cache
 def get_neighbours(pixel: tuple):
-	for dx, dy in [(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]:
-		yield (pixel[0] + dx, pixel[1] + dy)
+	neighbours = [(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]
+	return [(pixel[0] + dx, pixel[1] + dy) for dx, dy in neighbours]
 
 def enhance(img: dict, size: tuple, a: list, iteration: int) -> dict:
-	result = defaultdict(lambda: a[0] if iteration % 2 == 0 else False)
+	result = defaultdict(lambda: a[0] if iteration % 2 == 0 else 0)
 	min_x, max_x, min_y, max_y = size
 	for x in range(min_x, max_x+1):
 		for y in range(min_y, max_y+1):
-			binary = "".join(str(int(img[n])) for n in get_neighbours((x,y)))
+			binary = "".join(str(img[n]) for n in get_neighbours((x,y)))
 			result[(x,y)] = a[int(binary, 2)]
 	return result
 
@@ -22,23 +24,21 @@ def enhance(img: dict, size: tuple, a: list, iteration: int) -> dict:
 with open("input.txt") as file:
 	sections = file.read().split("\n\n")
 
-algorithm = [char == "#" for char in sections[0].strip()]
-image = defaultdict(lambda: False)
+algorithm = [int(char == "#") for char in sections[0].strip()]
+image = defaultdict(int)
 for y_coord, line in enumerate(sections[1].split("\n")):
 	for x_coord, char in enumerate(line):
-		image[(x_coord,y_coord)] = char == "#"
+		image[(x_coord,y_coord)] = int(char == "#")
 image_dimensions = (
 	0, max(image)[0],
-	0, max(image, key=lambda p: p[1])[1],
+	0, max(image, key=lambda p: p[1])[1]
 )
 
 images = [image]
 for i in range(50):
 	image_dimensions = (
-		image_dimensions[0]-1,
-		image_dimensions[1]+1,
-		image_dimensions[2]-1,
-		image_dimensions[3]+1,
+		image_dimensions[0]-1, image_dimensions[1]+1,
+		image_dimensions[2]-1, image_dimensions[3]+1
 	)
 	images.append(enhance(images[-1], image_dimensions, algorithm, i))
 
