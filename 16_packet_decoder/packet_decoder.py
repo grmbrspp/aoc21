9d7ceff83b522f727461525fa28ba61c04f866da
tim <tim.leonhard@posteo.de>
2022-01-05 18:30:34 +0100

    day16: use generator expression to create HEX_TO_BIN dictionary

--- 16_packet_decoder/packet_decoder.py
+++ 16_packet_decoder/packet_decoder.py
@@ -7,22 +7,8 @@ begin = time.time()
 
 Packet = namedtuple("Packet", ["version", "type", "sub_count", "value"])
 HEX_TO_BIN = {
-	"0": "0000",
-	"1": "0001",
-	"2": "0010",
-	"3": "0011",
-	"4": "0100",
-	"5": "0101",
-	"6": "0110",
-	"7": "0111",
-	"8": "1000",
-	"9": "1001",
-	"A": "1010",
-	"B": "1011",
-	"C": "1100",
-	"D": "1101",
-	"E": "1110",
-	"F": "1111",
+	f"{n:X}": f"{n:04b}"
+	for n in range(16)
 }
 
 def product(iteralbe) -> int:
